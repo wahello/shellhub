a4b93715c86629136d41b29c77058baea65b32e3
Otavio Salvador <otavio@ossystems.com.br>
2021-09-14 10:50:42 -0300

    api: Upgrade MongoDB to 5.0.2 release
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- api/pkg/dbtest/dbserver.go
+++ api/pkg/dbtest/dbserver.go
@@ -103,7 +103,7 @@ func (dbs *DBServer) start() {
 	}
 
 	args := []string{
-		"run", "--rm", fmt.Sprintf("--net=%s", dbs.network), "mongo:4.4.8",
+		"run", "--rm", fmt.Sprintf("--net=%s", dbs.network), "mongo:5.0.2",
 		"--bind_ip", "127.0.0.1",
 		"--port", strconv.Itoa(addr.Port),
 		"--nojournal",
